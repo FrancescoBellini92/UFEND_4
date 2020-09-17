4a0f2e1ea9c68ab902f4ffb29ba4d96c6fb13c34
Francesco Bellini <francescobellini92@gmail.com>
2020-09-17 22:48:16 +0200

    WIP add comment

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,4 @@ import '@babel/polyfill';
 import main from './js/app';
 import './scss/index.scss';
 
-(() => document.addEventListener('DOMContentLoaded', main))();
+(() => document.addEventListener('DOMContentLoaded', main))(); // using IIFE to encapsulate logic
